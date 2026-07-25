646e3a3d023b0b6353d617194575cd5760fabf74
Karim Oulad Chalha <ouladchalha.k@gmail.com>
2026-07-25 18:13:30 +0200

    Configure Google Analytics measurement ID

--- analytics-config.js
+++ analytics-config.js
@@ -1,5 +1,5 @@
 // Set this value to your Google Analytics 4 measurement ID, for example: G-ABC123DEF4.
 // Analytics remains disabled while this value is empty.
 window.siteConfig = {
-  googleAnalyticsMeasurementId: "",
+  googleAnalyticsMeasurementId: "G-34CMSJJ1SW",
 };
